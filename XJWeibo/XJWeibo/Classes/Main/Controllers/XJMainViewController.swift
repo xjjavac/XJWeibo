e8f6b09e00229293099a023339b0165642ae5923
xj <188383031@qq.com>
2016-08-26 15:38:33 +0800

    动态加载命名空间

--- XJWeibo/XJWeibo/Classes/Main/Controllers/XJMainViewController.swift
+++ XJWeibo/XJWeibo/Classes/Main/Controllers/XJMainViewController.swift
@@ -30,10 +30,10 @@ class XJMainViewController: UITabBarController {
         //3.将导航控制器添加到当前控制器上
         addChildViewController(nav)
         */
-        addChildViewController(XJHomeTableViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(XJMessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(XJDiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
-        addChildViewController(XJProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        addChildViewController("XJHomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("XJMessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("XJDiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+        addChildViewController("XJProfileTableViewController", title: "我", imageName: "tabbar_profile")
     }
     /**
      初始化只控制器
@@ -42,16 +42,32 @@ class XJMainViewController: UITabBarController {
      - parameter title:           子控制器的标题
      - parameter imageName:       子控制器的图片
      */
-    private func addChildViewController(childController:UIViewController,title:String,imageName:String) -> Void {
+//    private func addChildViewController(childController:UIViewController,title:String,imageName:String) -> Void {
+    private func addChildViewController(childControllerName:String,title:String,imageName:String) -> Void {
+//        <XJWeibo.XJHomeTableViewController: 0x7fc62341ef20>
+//        print(childController)
+        // -1.动态获取命名空间
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        //0.将字符串转换为类
+        //0.1默认情况下命名空间就是项目的名称,但是命名空间名称是可以修改的
+        let cls:AnyClass? = NSClassFromString("\(ns)." + childControllerName)
+        
+        //0.2 通过类创建对象
+        //0.2.1将anyClass转换为指定的类型
+        let vcCls = cls as! UIViewController.Type
+        let vc = vcCls.init()
+        
+       
+        
         
         //1.设置首页对应的数据
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        childController.title = "首页"
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.title = "首页"
         
         //2.给首页包装一个导航控制器
         let nav = UINavigationController()
-        nav.addChildViewController(childController)
+        nav.addChildViewController(vc)
         
         //3.将导航控制器添加到当前控制器上
         addChildViewController(nav)
